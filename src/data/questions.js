148abc4626f8eb03f39206d4d440bed94221fca2
Felipe Ribeiro <felips2ribeiro@gmail.com>
2024-07-11 12:44:19 -0300

    Criando dados das perguntas

---
+++ src/data/questions.js
@@ -0,0 +1,55 @@
+const questions = [
+    {
+      question: "Qual é a tag HTML correta para criar um parágrafo?",
+      options: ["A) <para>", "B) <p>", "C) <paragraph>", "D) <pg>"],
+      answer: "B) <p>"
+    },
+    {
+      question: "Como você adiciona um comentário em um arquivo HTML?",
+      options: ["A) <!-- Isto é um comentário -->", "B) // Isto é um comentário", "C) /* Isto é um comentário */", "D) <comment> Isto é um comentário </comment>"],
+      answer: "A) <!-- Isto é um comentário -->"
+    },
+    {
+      question: "Qual propriedade CSS é usada para mudar a cor de fundo de um elemento?",
+      options: ["A) color", "B) background-color", "C) bgcolor", "D) background"],
+      answer: "B) background-color"
+    },
+    {
+      question: "Como você escreve um comentário em CSS?",
+      options: ["A) // Isto é um comentário", "B) <!-- Isto é um comentário -->", "C) /* Isto é um comentário */", "D) # Isto é um comentário"],
+      answer: "C) /* Isto é um comentário */"
+    },
+    {
+      question: "Qual método JavaScript é usado para selecionar um elemento pelo seu ID?",
+      options: ["A) getElementById", "B) getElementByClass", "C) querySelector", "D) querySelectorAll"],
+      answer: "A) getElementById"
+    },
+    {
+      question: "Como você declara uma função em JavaScript?",
+      options: ["A) function minhaFuncao() {}", "B) def minhaFuncao() {}", "C) minhaFuncao() {}", "D) create function minhaFuncao() {}"],
+      answer: "A) function minhaFuncao() {}"
+    },
+    {
+      question: "Qual é a saída do seguinte código JavaScript?\nconsole.log(typeof 42);",
+      options: ["A) number", "B) string", "C) boolean", "D) undefined"],
+      answer: "A) number"
+    },
+    {
+      question: "Qual evento JavaScript é disparado quando o usuário clica em um elemento HTML?",
+      options: ["A) onchange", "B) onmouseover", "C) onclick", "D) onmouseclick"],
+      answer: "C) onclick"
+    },
+    {
+      question: "Qual é a sintaxe correta para referenciar um arquivo de estilo externo chamado 'style.css'?",
+      options: ["A) <stylesheet>style.css</stylesheet>", "B) <link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\">", "C) <style src=\"style.css\">", "D) <link src=\"style.css\" rel=\"stylesheet\">"],
+      answer: "B) <link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\">"
+    },
+    {
+      question: "Como você define uma variável em JavaScript?",
+      options: ["A) variable nome = valor;", "B) var nome = valor;", "C) v nome = valor;", "D) val nome = valor;"],
+      answer: "B) var nome = valor;"
+    }
+  ];
+  
+  export default questions;
+  
