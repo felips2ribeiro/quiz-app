ac9cc4b3a583f92e4b80d595bcab2d7cee641a0b
Felipe Ribeiro <felips2ribeiro@gmail.com>
2024-07-11 12:49:46 -0300

    Arrumando a função question

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-export default Question = ({ question, options, handleAnswer }) => {
+export default function Question ({ question, options, handleAnswer })  {
     return (
         <div>
             <h2>{question}</h2>
             <div>
-                {options.map((options, index) => (
+                {options.map((option, index) => (
                     <button key={index} onClick={() => handleAnswer(option)}>
                         {option}
                     </button>
