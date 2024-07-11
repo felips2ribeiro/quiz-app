f95ca45b3117c888c56e9db032a2e5f4125ce4e7
Felipe Ribeiro <felips2ribeiro@gmail.com>
2024-07-11 13:43:25 -0300

    Estilizando a tela de Resultados

--- src/components/Results.js
+++ src/components/Results.js
@@ -4,7 +4,7 @@ export default function Results ({ score, totalQuestions}) {
     const percentage = (score /totalQuestions ) * 100;
 
     return (
-        <div>
+        <div className="container">
             <h2>Resultados</h2>
             <p>Você acertou {score} de {totalQuestions} perguntas.</p>
             <p>Sua pontuação é {percentage}%</p>
